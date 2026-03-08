06cc1066df45c5d177b342c47847308272665610
MrNeon-0192 <mrneon925@gmail.com>
2026-03-08 19:17:56 +0600

    Type Casting

---
+++ Lesson 3 activity 2/jidhgt.py
@@ -0,0 +1,22 @@
+Name="Pingu"
+Age=(13)
+Weight=(36.5)
+is_student=True
+
+print("Name:",Name)
+print("Data type of Name is :", type(Name))
+print("Age:",Age)
+print("Data type of Age is :", type(Age))
+print("Weight:",Weight)
+print("Data type of Weight is :", type(Weight))
+print("Is Student:",is_student)
+print("Data type of Is Student is :", type(is_student))
+
+print("\n After Type Casting :\n")
+age=str(Age)
+print(age)
+print("Data type of Age after type casting is :", type(age))
+
+weight=int(Weight)
+print(weight)
+print("Data type of Weight after type casting is :", type(weight))

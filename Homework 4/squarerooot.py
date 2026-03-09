29dcd26689d282369334926c82171777b5724555
MrNeon-0192 <mrneon925@gmail.com>
2026-03-09 17:35:51 +0600

    fgrfrf

---
+++ Homework 4/squarerooot.py
@@ -0,0 +1,8 @@
+x=int(input("Enter a number to find its square root: "))
+
+
+if x<0:
+    print("The number can not be negative")
+else:
+    sqrt=x**0.5
+    print("The square root of", x, "is", sqrt)

3f96c33d504162bb489336a8c133d68bb3aaaad9
MrNeon-0192 <mrneon925@gmail.com>
2026-03-16 17:18:18 +0600

    ddd

---
+++ Lesson 6 activity 1/dfkjdfjividfhv.py
@@ -0,0 +1,22 @@
+a=10
+b=9
+c=0
+
+if a and b and c:
+    print("All the numbers has boolean value as True.")
+else:
+    print("At least one of the numbers has boolean value as False.")
+
+x=3
+y=-8
+z=0
+
+if x>0 or y>0:
+    print("At least one of the numbers is positive.")
+else:
+    print("Neither of the numbers is positive.")
+
+if y>0 or z>0:
+    print("At least one of the numbers is negative.")
+else:
+    print("Neither of the numbers is negative.")

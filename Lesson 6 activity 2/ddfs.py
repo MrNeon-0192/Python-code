cc2fbe1cfab30f5f2dd503795ca6e00b23290ce2
MrNeon-0192 <mrneon925@gmail.com>
2026-03-16 17:29:41 +0600

    rosjxg

---
+++ Lesson 6 activity 2/ddfs.py
@@ -0,0 +1,27 @@
+a=12
+b=7
+c=4
+
+print(a!=b)
+print(a!=c)
+print(b!=c)
+
+q="Hello"
+e="World"
+
+if q!=e:
+    print(q,"and", e, "are not the same.")
+
+m=5
+n=4
+
+if (m==7)!=(n==4):
+    print("Hi")
+
+z=int(input("Enter a number: "))
+
+if z%2==0:
+    print(z, "is an even number.")
+else:
+    print(z, "is not an even number.")
+

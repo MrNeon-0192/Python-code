8a9da88e2260e8640f8f86ccd18014d2c05e2436
MrNeon-0192 <mrneon925@gmail.com>
2026-03-30 17:18:02 +0600

    fgfg

---
+++ Les 8 act 4/poeppr.py
@@ -0,0 +1,20 @@
+a=int(input("Enter a value: "))
+b=int(input("Enter a value 2: "))
+c=int(input("Enter a value 3: "))
+avg=(a+b+c)/3
+print("avg: ", avg)
+
+if avg>a and avg>b and avg>c:
+    print("%d is higher than %d, %d and %d" % (avg, a, b, c))
+elif avg>a and avg>b:
+    print("%d is higher than %d and %d" % (avg, a, b))
+elif avg>a and avg>c:
+    print("%d is higher than %d and %d" % (avg, a, c))
+elif avg>b and avg>c:
+    print("%d is higher than %d and %d" % (avg, b, c))
+elif avg>a:
+    print("%d is just higher than %d" % (avg, a))
+elif avg>b:
+    print("%d is just higher than %d" % (avg, b))
+elif avg>c:
+    print("%d is just higher than %d" % (avg, c))

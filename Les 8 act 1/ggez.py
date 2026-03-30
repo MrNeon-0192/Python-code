bf901e6eb7706f0660f014e8ccb95ee1d123bec8
MrNeon-0192 <mrneon925@gmail.com>
2026-03-30 16:47:37 +0600

    ghvh

---
+++ Les 8 act 1/ggez.py
@@ -0,0 +1,13 @@
+a=4
+b=5
+c=7
+d=2
+
+print("The value of (a+b)*c/d is: ", (a+b)*c/d)
+
+name="Alex"
+age=0
+if (name=="Alex" or name=="John") and age>=2:
+    print("Hello World")
+else:
+    print("Goodbye World")

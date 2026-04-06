45f74f88bd44397557250607972a641396be4c66
MrNeon-0192 <mrneon925@gmail.com>
2026-04-06 17:23:44 +0600

    wedfgh

---
+++ Les 10 act 2/lingang.py
@@ -0,0 +1,5 @@
+string1=input("Please enter your own String: ")
+string2=""
+for i in string1:
+    string2=i+string2
+print("The reverse of the string is: ",string2)
---
+++ Les 10 act 3/ndhfg.py
@@ -0,0 +1,6 @@
+N=int(input("Enter a number: "))
+
+print("The number from {0} to {1} is: ".format(N,1))
+
+for i in range(N,0,-1):
+    print(i)
